22998b852050b34106e0570b63f6c85c419e8067
artxuan <slug168@gmail.com>
2019-08-22 12:14:08 +0000

    用户激活

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
